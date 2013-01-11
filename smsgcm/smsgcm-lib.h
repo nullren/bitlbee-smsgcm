aed33d5e887318a354d717e570f5f15c4bf56af7
nullren <chopflop@gmail.com>
2013-01-10 16:11:16 -0800

    added a few functions for bitlbee login and lotuout

--- smsgcm/smsgcm-lib.h
+++ smsgcm/smsgcm-lib.h
@@ -1,6 +1,9 @@
 #ifndef _SMSGCM_LIB_H_
 #define _SMSGCM_LIB_H_
 
+#define SMSGCM_API_URL "https://smsgcm.omgren.com"
 
+#define SMSGCM_SEND_MSG_URL "/sendMessage"
+#define SMSGCM_RECV_MSG_URL "/receiveMessage"
 
 #endif
--- smsgcm/smsgcm.c
+++ smsgcm/smsgcm.c
@@ -1,11 +1,66 @@
 #include "smsgcm.h"
 
+static void smsgcm_init(account_t *acc)
+{
+  //set some settings i guess
+  set_t *s;
+
+  char *def_url = SMSGCM_API_URL;
+
+  s = set_add(&acc->set, "fetch_interval", "60", set_eval_int, acc);
+  s->flags |= ACC_SET_OFFLINE_ONLY;
+
+  s = set_add(&acc->set, "base_url", def_url, NULL, acc);
+  s->flags |= ACC_SET_OFFLINE_ONLY;
+
+  s = set_add(&acc->set, "client_cert", "", NULL, acc);
+  s->flags |= ACC_SET_OFFLINE_ONLY;
+
+  s = set_add(&acc->set, "ca_cert", "", NULL, acc);
+  s->flags |= ACC_SET_OFFLINE_ONLY;
+
+}
+
+static void smsgcm_login(account_t *acc)
+{
+  struct im_connection *ic = imcb_new(acc);
+
+  // check that we can read or find the certs
+  char *client_path = set_getstr(&ic->acc->set, "client_cert");
+  char *ca_path = set_getstr(&ic->acc->set, "ca_cert");
+
+  // try to open certs
+  FILE *cl_file = fopen(client_path, "r");
+  FILE *ca_file = fopen(ca_path, "r");
+
+  if( cl_file == NULL ){
+    imcb_error(it, "Cannot open client credentials: %s", client_path);
+    imc_logout(ic, FALSE);
+  }
+
+  if( cl_file == NULL ){
+    imcb_error(it, "Cannot open CA certificate: %s", ca_path);
+    imc_logout(ic, FALSE);
+  }
+
+  fclose(cl_file);
+  fclose(ca_file);
+}
+
+static void smsgcm_logout(struct im_connection *ic)
+{
+  // really nothing to do
+}
+
 void init_plugin()
 {
   struct prpl *ret = g_new0(struct prpl, 1);
 
   ret->options = OPT_NOOTR;
   ret->name = "smsgcm";
+  ret->init = smsgcm_init;
+  ret->login = smsgcm_login;
+  ret->logout = smsgcm_logout;
 
   register_protocol(ret);
 }
