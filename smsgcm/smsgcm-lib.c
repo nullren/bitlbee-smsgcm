70e424ef5f173b7d0d005232cf4dbed0181826fc
nullren <chopflop@gmail.com>
2013-01-10 22:14:28 -0800

    i have no idea what i am doing

--- smsgcm/smsgcm-lib.c
+++ smsgcm/smsgcm-lib.c
@@ -40,6 +40,21 @@ int smsgcm_send_message(struct im_connection *ic, char *addr, char *msg)
   return r;
 }
 
+static void smsgcm_add_buddy(struct im_connection *ic, char *name, const char *phone)
+{
+  struct smsgcm_data *sd = ic->proto_data;
+
+  // Check if the buddy is already in the buddy list.
+  if (!bee_user_by_handle(ic->bee, ic, name)) {
+    imcb_log(ic, "add buddy for %s", name);
+    return;
+    imcb_add_buddy(ic, name, NULL);
+    imcb_rename_buddy(ic, name, phone);
+    imcb_buddy_nick_hint(ic, name, name);
+    imcb_buddy_status(ic, name, OPT_LOGGED_IN, NULL, NULL);
+  }
+}
+
 void smsgcm_load_messages(struct im_connection *ic)
 {
   // look at receiveMessage and download
@@ -83,9 +98,16 @@ void smsgcm_load_messages(struct im_connection *ic)
       continue;
     }
 
-    imcb_log(ic, "read message: %s (%s): %s", json_string_value(name)
-                                            , json_string_value(address)
-                                            , json_string_value(message));
+    struct message_data *msg = g_new0(struct message_data, 1);
+    msg->name = json_string_value(name);
+    msg->address = json_string_value(address);
+    msg->message = json_string_value(message);
+
+    imcb_log(ic, "read message: %s (%s): %s", msg->name
+                                            , msg->address
+                                            , msg->message);
+
+    smsgcm_add_buddy(ic, msg->name, msg->address);
   }
 
   json_decref(root);
--- smsgcm/smsgcm-lib.h
+++ smsgcm/smsgcm-lib.h
@@ -11,6 +11,13 @@
 
 #include <jansson.h>
 
+struct message_data {
+  char *name;
+  char *address;
+  char *message;
+  long time;
+};
+
 void smsgcm_load_messages(struct im_connection *ic);
 char *smsgcm_download_recent_messages(struct im_connection *ic);
 
