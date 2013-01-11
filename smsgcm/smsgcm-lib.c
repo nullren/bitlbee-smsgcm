d8b3afb8f5d63b02463384f720e8fe176b5e7c35
nullren <chopflop@gmail.com>
2013-01-10 18:45:19 -0800

    commented out buddy message thing

--- smsgcm/smsgcm-lib.c
+++ smsgcm/smsgcm-lib.c
@@ -2,4 +2,8 @@
 
 void smsgcm_load_messages(struct im_connection *ic)
 {
+  // look at receiveMessage and download
+
+  // doesn't work - need to add buddy first, probably
+  //imcb_buddy_msg(ic, "test", "lol", 0, 0);
 }
